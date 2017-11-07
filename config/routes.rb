9e5c0cdcc20e22f5305ea714b64390eeb630b6d2
Ryan Wiens <ryaninshanghai@gmail.com>
2017-11-07 15:50:30 +0100

    configured routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
-  get 'articles/index'
 
-  get 'articles/show'
 
-  get 'articles/new'
+  get "articles", to: "articles#index"
 
-  get 'articles/create'
+  get "articles/new", to: "articles#new"
+  post "articles", to: "articles#create"
 
-  get 'articles/edit'
+  get "articles/:id", to: "articles#show"
 
-  get 'articles/update'
 
-  get 'articles/destroy'
+  get "articles/:id/edit", to: "articles#edit"
+  patch "articles/:id", to: "articles#update"
+
+  delete "articles/:id", to: "articles#destroy"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
