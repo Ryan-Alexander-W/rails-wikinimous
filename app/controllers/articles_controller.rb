f8e39ecec771550b606c7215ac52dfaa5f03e192
Ryan Wiens <ryaninshanghai@gmail.com>
2017-11-07 17:21:38 +0100

    built models routes and views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,22 +1,54 @@
 class ArticlesController < ApplicationController
+before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def index
+    @articles = Article.all
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article =Article.new(article_params)
+    @article.save
+
+   redirect_to article_path(@article)
   end
 
   def edit
+
   end
 
   def update
+     @article.update(article_params)
+     redirect_to article_path(@article)
   end
 
   def destroy
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+  private
+
+ def set_article
+  @article = Article.find(params[:id])
+ end
+
+ def article_params
+    # params
+    # => { id: '1', restaurant: { name: "Loetjes", address: "Amsterdam" } }
+
+    # params.require(:restaurant)
+    # => { name: "Loetjes", address: "Amsterdam", featured: true }
+
+    params.require(:article).permit(:title, :content)
+    # => { name: "Loetjes", address: "Amsterdam" }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
 
   get "articles", to: "articles#index"
 
-  get "articles/new", to: "articles#new"
+  get "articles/new", to: "articles#new", as: "new_article"
   post "articles", to: "articles#create"
 
-  get "articles/:id", to: "articles#show"
+  get "articles/:id", to: "articles#show", as: "article"
 
 
-  get "articles/:id/edit", to: "articles#edit"
+  get "articles/:id/edit", to: "articles#edit", as: "edit_article"
   patch "articles/:id", to: "articles#update"
 
   delete "articles/:id", to: "articles#destroy"
